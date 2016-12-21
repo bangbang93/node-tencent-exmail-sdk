36579e6228116f2bf89d6a33786e64baef59a33e
bangbang93 <bangbang93@163.com>
2016-12-21 18:12:04 +0800

    获取未读邮件数

--- mail.js
+++ mail.js
@@ -2,3 +2,34 @@
  * Created by bangbang93 on 2016/12/21.
  */
 'use strict';
+const Api = require('./api');
+const makeError = require('./util').makeError;
+
+class Mail extends Api {
+
+  /**
+   *
+   * @param userId
+   * @param beginDate
+   * @param endDate
+   * @returns {Promise.<Number>}
+   */
+  newCount(userId, beginDate, endDate) {
+    return this.request.get('/cgi-bin/mail/newcount', {
+      qs: {
+        access_token: this.sdk.accessToken,
+      },
+      form: {
+        userid: userId,
+        begin_date: beginDate,
+        end_date: endDate,
+      }
+    })
+      .then((res)=>{
+        if (res.body.errcode){
+          throw makeError(res.body);
+        }
+        return res.body.count;
+      })
+  }
+}
